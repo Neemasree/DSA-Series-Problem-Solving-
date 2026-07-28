81deb643f5e0e96e798d92ec3913e932934165b0
Neemasree <neemasree04@gmail.com>
2026-07-28 22:07:29 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.8%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power=abs((long long)n);
+        double ans=1.0;
+        if(n<0){
+            x=1.0/x;
+        }
+        while(power>0){
+            if(power%2==1)
+                ans*=x;
+                x*=x;
+            power/=2;
+        }
+        return ans;
+    }
+};
