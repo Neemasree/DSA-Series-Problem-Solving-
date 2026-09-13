1374a2c1cd7d99cafca544f6c11ff8b19c194595
Neemasree <neemasree04@gmail.com>
2026-09-13 21:18:40 +0530

    Time: 0 ms (100%), Space: 9.5 MB (10.86%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n=s.size();
+        stack<int>st;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push(s[i]);  
+            }else{
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                }
+                else{
+                    ans++;
+                }
+            }
+        }
+        ans+=st.size();
+        return ans;
+    }
+};
